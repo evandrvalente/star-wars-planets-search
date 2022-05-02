ad4ed7995e6d09956e0af0be1b572c9b44b1c2d9
Evandro Valente <evandr.valente@gmail.com>
2022-05-02 13:09:15 -0300

    add filters by numeric values

--- src/components/PlanetsFilters.js
+++ src/components/PlanetsFilters.js
@@ -1,14 +1,27 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 // setar um estado para os planetas encontrados após filtros
 
 function PlanetsFilters() {
-  const { planets, setFiltersResult } = useContext(PlanetsContext);
-  // const [planetName, setPlanetName] = useState('');
+  const { planets, filtersResult, setFiltersResult } = useContext(PlanetsContext);
+  const [activeFilters, setActiveFilters] = useState([]);
   const [filterByName, setFilterByName] = useState({
     name: '',
   });
+
+  const [filterByNumericValues, setFilterByNumericValues] = useState({
+    column: '',
+    comparison: '',
+    value: 0,
+  });
+
+  const columnOptions = [
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water'];
 
   const filterPlanetName = (value) => {
     const planetsListbyName = planets.filter(({ name }) => name.includes(value));
@@ -20,14 +33,143 @@ function PlanetsFilters() {
     filterPlanetName(value);
   };
 
+  const tratarDados = (linha) => {
+    const bools = [];
+
+    activeFilters.forEach((filter) => {
+      switch (filter.comparison) {
+      case '>':
+        bools.push(Number(linha[filter.column]) >= Number(filter.value));
+        break;
+      case '<':
+        bools.push(Number(linha[filter.column]) <= Number(filter.value));
+        break;
+      case '=':
+        bools.push(linha[filter.column] === filter.value.toUpperCase());
+        break;
+      default:
+        return true;
+      }
+    });
+    return bools.every((el) => el);
+  };
+
+  // const updateTableData = () => {
+  //   const newTableData = filtersResult.filter(tratarDados);
+  //   setFiltersResult(newTableData);
+  // };
+
+  useEffect(() => {
+    console.log(activeFilters);
+    console.log(filtersResult);
+  }, [activeFilters, filtersResult]);
+
+  const handleFilterInput = ({ target: { name, value } }) => {
+    setFilterByNumericValues({ ...filterByNumericValues, [name]: value });
+  };
+
+  const handleOptions = (option) => !activeFilters
+    .find((filtro) => option === filtro.column);
+
   return (
-    <form>
-      <input
-        data-testid="name-filter"
-        value={ filterByName.name }
-        onChange={ handleNameChange }
-      />
-    </form>
+    <div>
+      <form>
+        <input
+          data-testid="name-filter"
+          value={ filterByName.name }
+          onChange={ handleNameChange }
+        />
+        <select
+          data-testid="column-filter"
+          name="column"
+          value={ filterByNumericValues.column }
+          onChange={ handleFilterInput }
+        >
+          <option value="">Selecione uma opção</option>
+          {columnOptions
+            .filter(handleOptions)
+            .map((column) => (
+              <option value={ column } key={ column }>
+                {column}
+              </option>
+            ))}
+        </select>
+        <select
+          data-testid="comparison-filter"
+          name="comparison"
+          value={ filterByNumericValues.comparison }
+          onChange={ handleFilterInput }
+        >
+          <option value="">Selecione uma opção</option>
+          <option value=">">maior que </option>
+          <option value="<">menor que </option>
+          <option value="=">igual a </option>
+        </select>
+        <input
+          data-testid="value-filter"
+          type="number"
+          name="value"
+          value={ filterByNumericValues.value }
+          onChange={ handleFilterInput }
+        />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ () => {
+            setActiveFilters([...activeFilters, filterByNumericValues]);
+            // updateTableData();
+            const newTableData = filtersResult.filter(tratarDados);
+            setFiltersResult(newTableData);
+            setFilterByNumericValues({
+              column: '',
+              comparison: '',
+              value: '',
+            });
+          } }
+        >
+          FILTRAR
+        </button>
+        <button
+          type="button"
+          data-testid="button-remove-filters"
+          onClick={ () => {
+            setActiveFilters([]);
+            setFilterByNumericValues({
+              column: '',
+              comparison: '',
+              value: '',
+            });
+            setFiltersResult(planets);
+          } }
+        >
+          LIMPAR FILTROS
+        </button>
+      </form>
+      {activeFilters.map((filter, index) => (
+        <div
+          className="filters"
+          data-testid="filter"
+          key={ index }
+        >
+          <button
+            type="button"
+            className="limpar"
+            onClick={ () => {
+              const cloneArray = [...activeFilters];
+              cloneArray.splice(index, 1);
+              setActiveFilters(cloneArray);
+            } }
+          >
+            X
+          </button>
+          {filter.column}
+          {' '}
+          {filter.condition}
+          {' '}
+          {filter.value}
+        </div>
+      ))}
+    </div>
   );
 }
 
